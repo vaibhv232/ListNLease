d7fec6667a767502b0abdac9610725699a6e2182
Vaibhav Singh <21bcs232@iiitdmj.ac.in>
2023-10-26 10:37:09 +0530

    added all required files

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About ListNLease
+      </h1>
+      <p className="mb-4 text-slate-700">
+        ListNLease is your premier real estate platform, specializing in helping
+        clients buy, sell, and rent properties in the most desirable
+        neighborhoods. Our dedicated team of experienced agents is committed to
+        delivering exceptional service, ensuring a seamless buying and selling
+        process.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission at ListNLease is to empower our clients to achieve their
+        real estate goals through expert advice, personalized service, and an
+        in-depth understanding of the local market. Whether you're looking to
+        buy, sell, or rent a property, we're here to support you every step of
+        the way.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our team of agents brings a wealth of experience and knowledge to the
+        real estate industry, and we're unwavering in our commitment to
+        providing the highest level of service to our clients. We believe that
+        the process of buying or selling a property should be an exciting and
+        rewarding experience, and ListNLease is dedicated to making that a
+        reality for each and every one of our clients.
+      </p>
+    </div>
+  );
+}
