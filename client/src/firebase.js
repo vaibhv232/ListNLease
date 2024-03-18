7633d87924f79e3f37b3e1b623d49454d889987e
vaibhv232 <vaibhav.official.232@gmail.com>
2024-03-19 01:03:58 +0530

    Done with firebase storage

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  apiKey: "AIzaSyD7lnlqlWJWJRedaeLncx7OoC7CmT_R62w",
+  authDomain: "listnlease.firebaseapp.com",
+  projectId: "listnlease",
+  storageBucket: "listnlease.appspot.com",
+  messagingSenderId: "742301304571",
+  appId: "1:742301304571:web:12cbc9362f4ee6d33ed25e",
+  measurementId: "G-W831BY45F3"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
       },
     },
   },
-
   plugins: [react()],
 });
 
